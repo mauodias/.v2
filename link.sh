20967bfea7b4ad76172459d832dfdcd87f795a8a
Maurício Dias <mau.dias@gmail.com>
2018-12-16 01:07:28 -0200

    Update link function

--- link.sh
+++ link.sh
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-for i in *.symlink; do
-  if [ ! -e ~/.$(basename $i .symlink) ]; then
-    ln -s $PWD/$i ~/.$(basename $i .symlink)
-    echo Linked $PWD/$i to ~/.$(basename $i .symlink)
-  else
-    echo .$(basename $i .symlink) exists in $HOME
-  fi
-done
+count=`ls -1 *.symlink 2>/dev/null | wc -l`
+if [ $count != 0 ]; then
+  for i in *.symlink; do
+    file=.$(basename $i .symlink)
+    if [ ! -e ~/$file ]; then
+      ln -s $PWD/$i ~/$file
+      echo Linked $PWD/$i to ~/$file
+    else
+      echo $file exists in $HOME
+    fi
+  done
+fi
 
 echo Finished linking dotfiles
