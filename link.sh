7d49bb199030167f59d18c196003c318eaee8aaa
Maurício Dias <mau.dias@gmail.com>
2018-12-16 14:38:57 -0200

    Add NVIM specific linking rules

--- link.sh
+++ link.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo
+echo - Linking general dotfiles
+echo
+
 count=`ls -1 *.symlink 2>/dev/null | wc -l`
 if [ $count != 0 ]; then
   for i in *.symlink; do
@@ -12,6 +16,28 @@ if [ $count != 0 ]; then
     fi
   done
 fi
+
+echo
+echo - Linking NVIM config files
+echo
+
+NVIMFOLDER=~/.config/nvim
+NVIMINIT=~/.config/nvim/vim.init
+
+if [ -e $NVIMFOLDER ]; then
+  ln -s $NVIMFOLDER ~/.vim
+  echo Linked $NVIMFOLDER to ~/.vim
+else
+  echo NVIM config \($NVIMFOLDER\) folder not found
+fi
+
+if [ -e $NVIMINIT ]; then
+  ln -s $NVIMINIT ~/.vimrc
+  echo Linked $NVIMINIT to ~/.vimrc
+else
+  echo NVIM init file \($NVIMINIT\) not found
+fi
+
 
 echo
 echo - Finished linking dotfiles
